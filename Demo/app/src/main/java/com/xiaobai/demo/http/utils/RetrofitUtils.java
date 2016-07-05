7f1b5fb247adb7994a120d9bace10ba512349df0
xiaobai <leejing0306@163.com>
2016-07-05 15:00:00 +0800

    设置公共头，公共参数

--- Demo/app/src/main/java/com/xiaobai/demo/http/utils/RetrofitUtils.java
+++ Demo/app/src/main/java/com/xiaobai/demo/http/utils/RetrofitUtils.java
@@ -1,8 +1,15 @@
 package com.xiaobai.demo.http.utils;
 
+import android.util.Log;
+
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
@@ -25,6 +32,7 @@ public class RetrofitUtils {
                     //手动创建一个OkHttpClient并设置超时时间
                     OkHttpClient.Builder builder = new OkHttpClient.Builder();
                     builder.connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
+                    builder.addInterceptor(addQueryParameterInterceptor());
                     // print Log
                     HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
                     logging.setLevel(HttpLoggingInterceptor.Level.BODY);
@@ -41,6 +49,53 @@ public class RetrofitUtils {
         }
         return singleton.create(clazz);
     }
+
+
+
+    /**
+     * 设置公共参数
+     */
+    private static Interceptor addQueryParameterInterceptor() {
+        Interceptor addQueryParameterInterceptor = new Interceptor() {
+            @Override
+            public Response intercept(Interceptor.Chain chain) throws IOException {
+                Request originalRequest = chain.request();
+                Request request;
+                HttpUrl modifiedUrl = originalRequest.url().newBuilder()
+                        // Provide your custom parameter here
+                        .addQueryParameter("platform", "android"+System.currentTimeMillis())
+                        .addQueryParameter("version", "1.0.0")
+                        .build();
+                request = originalRequest.newBuilder().url(modifiedUrl).build();
+                Log.e("tag",modifiedUrl.toString());
+                return chain.proceed(request);
+            }
+        };
+        return addQueryParameterInterceptor;
+    }
+
+
+    /**
+     * 设置头
+     */
+    private static Interceptor addHeaderInterceptor() {
+        Interceptor headerInterceptor = new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request originalRequest = chain.request();
+                Request.Builder requestBuilder = originalRequest.newBuilder()
+                        // Provide your custom header here
+                        .header("AppType", "TPOS")
+                        .header("Accept", "application/json")
+                        .method(originalRequest.method(), originalRequest.body());
+                Request request = requestBuilder.build();
+                return chain.proceed(request);
+            }
+        };
+        return headerInterceptor;
+    }
+
+
 
 
 }
